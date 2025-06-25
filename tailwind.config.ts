bdcde2d63d842988e3cf61190480f57ac346e2af
DigQuestor <danishnest@gmail.com>
2025-06-25 23:20:24 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,12 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+  "./client/src/**/*.{ts,tsx}",
+  "./client/pages/**/*.{ts,tsx}",
+  "./client/components/**/*.{ts,tsx}",
+  "./client/app/**/*.{ts,tsx}",
+],
   theme: {
     extend: {
       borderRadius: {
