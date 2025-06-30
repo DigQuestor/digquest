3847b15c5eab6ca5733e9395011ce843b623c327
DigQuestor <danishnest@gmail.com>
2025-07-01 01:13:05 +0200

    Update vite.config.minimal.ts

--- vite.config.minimal.ts
+++ vite.config.minimal.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
 export default defineConfig({
   plugins: [react()],
   root: ".",
@@ -17,7 +16,7 @@ export default defineConfig({
     outDir: "dist/client",
     emptyOutDir: true,
     rollupOptions: {
-  input: "./index.html",
+      input: "./index.html",
     },
   },
 });
