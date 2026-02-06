59bb8d864cc7d2cb2d73d0a397e355997e7501e3
DigQuestor <danishnest@gmail.com>
2026-02-06 18:45:58 +0100

    Create globals.d.ts

---
+++ client/src/types/globals.d.ts
@@ -0,0 +1,15 @@
+// globals.d.ts
+declare module 'lucide-react';
+declare module 'cmdk';
+declare module 'vaul';
+declare module 'input-otp';
+declare module 'tailwind-merge';
+declare module 'react-helmet';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.webp';
+declare module '*.svg';
+declare module '*.gif';
+declare module '*.css';
+declare module '*.scss';
